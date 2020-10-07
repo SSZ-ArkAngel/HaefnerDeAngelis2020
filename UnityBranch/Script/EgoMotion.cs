feb2ba35f14f4c4a91211fdc9167c976861d4222
SSZ-ArkAngel <72346382+SSZ-ArkAngel@users.noreply.github.com>
2020-10-07 16:54:48 +0900

    Add files via upload

---
+++ UnityBranch/Script/EgoMotion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EgoMotion : MonoBehaviour
+{
+    public float egoVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = new Vector3(0.0f, 0.0f, egoVelocity);
+        Vector3 displacement = velocity * Time.deltaTime;
+        transform.localPosition += displacement;
+    }
+}
