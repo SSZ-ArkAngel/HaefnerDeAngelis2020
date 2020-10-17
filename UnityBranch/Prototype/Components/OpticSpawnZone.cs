bd77ab374adce9dcf5b24eaf00eaf6f3af6a6913
SSZ-ArkAngel <72346382+SSZ-ArkAngel@users.noreply.github.com>
2020-10-17 22:53:53 +0900

    Add files via upload

---
+++ UnityBranch/Prototype/Components/OpticSpawnZone.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The SpawnZone script seeks to define spatial coordinates within which objects may be instantiated
+// While configurable replication suggests a 2M radius centered at the camera as a no-spawn-zone
+
+public class OpticSpawnZone : SpawnZone
+{
+
+    // The inner and outer radii of the concentric cylinder in which objects will be instantiated
+    public float FlowInnerLimit;
+    public float FlowOuterLimit;
+    // The length of the optic flow field
+    public float FlowFieldLength;
+    // The point at which optic flow can start to appear
+    public float FlowStartPoint;
+
+    public Transform prefab;
+    public int cloudParticleCount;
+
+    // Will attempt to return spatial coordinates as potential instantiation point candidates
+    public override Vector3 SpawnPoint
+    {
+        get
+        {
+            Vector3 p = Vector3.zero;
+
+            // The while loop will exit once the spawn point is within the defined bounds
+            //bool inBounds = false;
+            //while (inBounds = false)
+            //{
+            //    // The XY coordinates directly reflect the radius
+            //    p.x = Random.Range(-FlowOuterLimit, FlowOuterLimit);
+            //    p.y = Random.Range(-FlowOuterLimit, FlowOuterLimit);
+
+            //    if((p.x > FlowInnerLimit || p.x < -FlowInnerLimit) && (p.y > FlowInnerLimit || p.y < -FlowInnerLimit))
+            //    {
+            //        inBounds = true;
+            //    }
+            //}
+
+
+            p.x = Random.Range(-FlowOuterLimit, FlowOuterLimit);
+            p.y = Random.Range(-FlowOuterLimit, FlowOuterLimit);
+
+            //bool posneg = false;
+            //int multiplier = 1;
+            //float booltest = Random.Range(0.0f, 1.0f);
+            //if (booltest >= 0.5f)
+            //{
+            //    posneg = true;
+            //}
+            //else posneg = false;
+
+            //if(posneg == true)
+            //{
+            //    multiplier = 1;
+            //}
+            //if(posneg == false)
+            //{
+            //    multiplier = -1;
+            //}
+
+            //p.x = multiplier * Random.Range(FlowInnerLimit, FlowOuterLimit);
+            //p.y = multiplier * Random.Range(FlowInnerLimit, FlowOuterLimit);
+
+            //float xposneg = Random.Range(0.0f, 1.0f);
+            //int xmult = 0;
+            //int ymult = 0;
+            //if(xposneg <= 0.5f)
+            //{
+            //    xmult = 1;
+            //}
+            //if(xposneg > 0.5f)
+            //{
+            //    xmult = -1;
+            //}
+
+            //float yposneg = Random.Range(0.0f, 1.0f);
+            //if (yposneg <= 0.5f)
+            //{
+            //    ymult = 1;
+            //}
+            //if (yposneg > 0.5f)
+            //{
+            //    ymult = -1;
+            //}
+
+            //p.x = xmult * Random.Range(FlowInnerLimit, FlowOuterLimit);
+            //p.y = ymult * Random.Range(FlowInnerLimit, FlowOuterLimit);
+
+            // The Z coordinate is reflective of the length
+            p.z = Random.Range(FlowStartPoint, FlowFieldLength);
+
+            return transform.TransformPoint(p);
+
+        }
+    }
+
+    private void Awake()
+    {
+        for (int i = 1; i <= cloudParticleCount; i++)
+        {
+            InstantiateFlow();
+        }
+    }
+
+    void InstantiateFlow()
+    {
+        Transform t = Instantiate(prefab);
+        t.localPosition = SpawnPoint;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityBranch/Prototype/Scripts/EgoMotion.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EgoMotion : MonoBehaviour
+{
+    public float egoVelocity;
+
+    // Fetch the coordinates for the probe's location
+    public Vector3 probeTransform;
+
+    public Transform probePrefab;
+    // Start is called before the first frame update
+
+    public GameObject cameraController;
+
+    public bool spawnProbe;
+    
+    void Awake()
+    {
+        // Instantiate a probe at the presented coordinates
+        if(spawnProbe == true)
+        {
+        Transform probe = Instantiate(probePrefab);
+        probe.localPosition = probeTransform;
+        probe.transform.parent = cameraController.transform;  
+        }
+
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = new Vector3(0.0f, 0.0f, egoVelocity);
+        Vector3 displacement = velocity * Time.deltaTime;
+        transform.localPosition += displacement;
+    }
+}
