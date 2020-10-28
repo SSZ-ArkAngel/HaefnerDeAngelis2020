69a96e87a91aba607513e40e6d76dbb89a34139b
SSZ-ArkAngel <72346382+SSZ-ArkAngel@users.noreply.github.com>
2020-10-28 19:09:24 +0900

    Shaders and Scripts

---
+++ UnityBranch/Beta/Scripts/Overlord/Overlord.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Overlord : MonoBehaviour
+{
+    public int sceneIndex;
+    public string conditionName;
+    public float probeStartLocation; // is always on the Z axis so Y=0
+    public float probeEndLocationX;
+    public float probeEndLocationY;
+    public float probeAngle;
+    public float probeVelX;
+    public float probeVelY;
+    public float apertureDegree;
+    public float absoluteTilt;
+    public float relativeTilt;
+    public float probeEccentricity;
+    public float radialFlowAtStart;
+    public float radialFlowAtEnd;
+    public float averageRadialFlow;
+    public float reactionTime;
+    public float stimulationTime;
+    
+    
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
